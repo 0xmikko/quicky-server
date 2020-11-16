3f631e69cbb1bc2a7ce9f51eafe52b97c02533de
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-11-16 20:37:09 +0300

    update

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "quicky-server",
     script: "./build/index.js",
     node_args: "-r dotenv/config",
-    instances: "4",
+    instances: "1",
     env: {
       NODE_ENV: "development",
     },
